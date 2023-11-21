0c0947fb71c2e9df3ee0a8f009b64e3e52453834
sebastian461 <sebastiano-461@hotmail.com>
2023-11-21 15:00:56 -0500

    Load users by page
    The HTTP request to the server was programmed through environment variables to load users to the client

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
+import { UsersApp } from "./src/users/users-app";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -15,3 +16,6 @@ document.querySelector("#app").innerHTML = `
     </div>
   </div>
 `;
+
+const element = document.querySelector(".card");
+UsersApp(element);
