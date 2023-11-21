5ba42fae66db34199d15cbad5bd9ed41b8dbc641
sebastian461 <sebastiano-461@hotmail.com>
2023-11-21 14:42:38 -0500

    Store scheme
    The base structure of the store was programmed

---
+++ src/users/store/users-store.js
@@ -0,0 +1,29 @@
+const state = {
+  currentPage: 0,
+  users: [],
+};
+
+const loadNextPage = async () => {
+  throw new Error("Not implemented");
+};
+
+const loadPreviousPage = async () => {
+  throw new Error("Not implemented");
+};
+
+const onUserChanged = () => {
+  throw new Error("Not implemented");
+};
+
+const reloadPage = async () => {
+  throw new Error("Not implemented");
+};
+
+export default {
+  loadNextPage,
+  loadPreviousPage,
+  onUserChanged,
+  reloadPage,
+  getUser: () => [...state.users],
+  getCurrentpage: () => state.currentPage,
+};
